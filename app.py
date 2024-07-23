baaef52000940955f40a355d1596e2c3c6827a81
Yoel Moghos <moghosy@gmail.com>
2024-07-24 00:20:37 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+import stripe
+
+app = Flask(__name__)
+
+# Use a test secret key from Stripe for demo purposes
+stripe.api_key = 'pk_test_51Of6goKZ0oo6AUWAwq11Y92ot4HUjcmfnW10x5absTA0SO2dQwRXmU608gHoYyyso1ID1wyX7sWKpSWPFA24UiQz00jg2UOGkT'
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = request.headers.get('Origin', 'https://try-design-team.webflow.io/')
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    return response
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    # Mock data for demo purposes
+    return jsonify({'id': 'cs_test_...'})
+
+if __name__ == '__main__':
+    app.run(port=4242)
