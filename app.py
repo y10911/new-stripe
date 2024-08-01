0d7bcd93983d8a16fcb6c80968b24266d14eb148
Yoel Moghos <moghosy@gmail.com>
2024-08-02 00:18:35 +0100

    secret key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import stripe
 app = Flask(__name__)
 
 # Use a test secret key from Stripe for demo purposes
-stripe.api_key = 'sk_test_51Of6goKZ0oo6AUWAhVAGBbJgiaBVpVUkPLBQOdVE4RKnE38lsscMVc85p2j0s6EjAD0bBOxz4pUIAh13t3UwWiTe00kHiko9gx'
+stripe.api_key = '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'
 
 @app.after_request
 def add_cors_headers(response):
